5e103fc72521c991293a21119c7afcc36293d786
SaileshRokaya <saileshrokaya123@gmail.com>
2024-05-17 14:25:56 +0545

    Shopping Cart Implementation, Using Modals

---
+++ FoodOrdering/src/app/cart.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "../components/Themed";
+import React from "react";
+import { Platform } from "react-native";
+
+const CartScreen = ()=>{
+    return(
+        <View>
+            <Text>cart</Text>
+            <StatusBar style={Platform.OS =='ios'? 'light': 'auto'}/>
+        </View>
+    )
+};
+
+export default CartScreen;
