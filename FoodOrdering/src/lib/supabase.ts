debb111d2d63760d3176bd4abc4cc700b3c34de0
SaileshRokaya <saileshrokaya123@gmail.com>
2024-05-23 17:46:48 +0545

    Creating and Configuring Supabase

---
+++ FoodOrdering/src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://cbldojcqirpxasqkitbl.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNibGRvamNxaXJweGFzcWtpdGJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY0NjUyODgsImV4cCI6MjAzMjA0MTI4OH0.PGt-8QYYAFeoXycLlbBvaBxdKFAf1HYOJtxd8qyrGn4';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
